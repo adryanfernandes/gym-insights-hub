862b37ae75bae1c0950a547591965b5dbeb9c971
Adryan <adryanfernandes@outlook.com>
2026-06-16 15:12:29 -0300

    att

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -50,7 +50,7 @@ export function DashboardLayout({
 
   async function handleSignOut() {
     await signOut();
-    navigate({ to: "/auth", replace: true });
+    navigate({ to: "/", replace: true });
   }
 
   return (
